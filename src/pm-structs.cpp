57e908863590227b6325f23b4c554e80c51836bf
HoneyHazard <8847050+HoneyHazard@users.noreply.github.com>
2020-04-03 02:14:56 -0700

    drafted code for loading a match config

--- src/pm-structs.cpp
+++ src/pm-structs.cpp
@@ -8,7 +8,7 @@ uint qHash(const OBSWeakSource &ws)
 
 bool PmMatchConfig::operator!=(const PmMatchConfig &other) const
 {
-    // TODO: hacky! clean up.
+    // TODO: hacky! clean up.somehow.
     if (this->matchImgFilename != other.matchImgFilename) {
         return true;
     } else {
@@ -19,7 +19,48 @@ bool PmMatchConfig::operator!=(const PmMatchConfig &other) const
 
 PmMatchConfig::PmMatchConfig(obs_data_t *data)
 {
-    // TODO
+    obs_data_set_default_string(
+        data, "match_image_filename", matchImgFilename.data());
+    matchImgFilename = obs_data_get_string(data, "match_image_filename");
+
+    obs_data_set_default_int(data, "roi_left", roiLeft);
+    roiLeft = int(obs_data_get_int(data, "roi_left"));
+
+    obs_data_set_default_int(data, "roi_bottom", roiBottom);
+    roiBottom = int(obs_data_get_int(data, "roi_bottom"));
+
+    obs_data_set_default_double(
+        data, "per_pixel_allowed_error", double(perPixelErrThresh));
+    perPixelErrThresh
+        = float(obs_data_get_double(data, "per_pixel_allowed_error"));
+
+    obs_data_set_default_double(
+        data, "total_match_threshold", double(totalMatchThresh));
+    totalMatchThresh =float(obs_data_get_double(data, "total_match_threshold"));
+
+    obs_data_set_default_int(data, "mask_mode", int(maskMode));
+    maskMode = PmMaskMode(obs_data_get_int(data, "mask_mode"));
+
+    obs_data_set_default_int(data, "custom_color", customColor);
+    customColor = uint32_t(obs_data_get_int(data, "custom_color"));
+
+    obs_data_set_default_int(data, "preview_mode", int(previewMode));
+    previewMode = PmPreviewMode(obs_data_get_int(data, "preview_mode"));
+
+    obs_data_set_default_double(
+        data, "preview_video_scale", double(previewVideoScale));
+    previewVideoScale
+        = float(obs_data_get_double(data, "preview_video_scale"));
+
+    obs_data_set_default_double(
+        data, "preview_region_scale", double(previewRegionScale));
+    previewRegionScale
+        = float(obs_data_get_double(data, "preview_region_scale"));
+
+    obs_data_set_default_double(
+        data, "preview_match_image_scale", double(previewMatchImageScale));
+    previewMatchImageScale
+        = float(obs_data_get_double(data, "preview_match_image_scale"));
 }
 
 obs_data_t* PmMatchConfig::save(const std::string &presetName) const
@@ -29,15 +70,25 @@ obs_data_t* PmMatchConfig::save(const std::string &presetName) const
     obs_data_set_string(ret, "match_image_filename", matchImgFilename.data());
     obs_data_set_int(ret, "roi_left", roiLeft);
     obs_data_set_int(ret, "roi_bottom", roiBottom);
-    obs_data_set_double(ret, "per_pixel_allowed_error", double(perPixelErrThresh));
-    obs_data_set_double(ret, "total_match_threshold", double(totalMatchThresh));
+    obs_data_set_double(
+        ret, "per_pixel_allowed_error", double(perPixelErrThresh));
+    obs_data_set_double(
+        ret, "total_match_threshold", double(totalMatchThresh));
     obs_data_set_int(ret, "mask_mode", int(maskMode));
     obs_data_set_int(ret, "custom_color", customColor);
     obs_data_set_int(ret, "preview_mode", int(previewMode));
-    obs_data_set_double(ret, "preview_video_scale", double(previewVideoScale));
-    obs_data_set_double(ret, "preview_region_scale", double(previewRegionScale));
-    obs_data_set_double(ret, "preview_match_image_scale", double(previewMatchImageScale));
+    obs_data_set_double(
+        ret, "preview_video_scale", double(previewVideoScale));
+    obs_data_set_double(
+        ret, "preview_region_scale", double(previewRegionScale));
+    obs_data_set_double(
+        ret, "preview_match_image_scale", double(previewMatchImageScale));
     return ret;
+}
+
+PmSwitchConfig::PmSwitchConfig(obs_data_t *data)
+{
+    // TODO
 }
 
 obs_data_t *PmSwitchConfig::save() const
