6382b0919e037aee38af6f8ed09d4c526fa6c2c6
HoneyHazard <8847050+HoneyHazard@users.noreply.github.com>
2020-09-28 18:16:24 -0700

    correction

--- src/pm-switcher.c
+++ src/pm-switcher.c
@@ -4,14 +4,14 @@ OBS_DECLARE_MODULE()
 
 OBS_MODULE_USE_DEFAULT_LOCALE("pixel-match-switcher", "en-US");
 
-extern struct pixel_match_filter_data pixel_match_filter;
+extern struct obs_source_info pixel_match_filter;
 
 extern void init_pixel_match_switcher(void);
 extern void free_pixel_match_switcher(void);
 
 bool obs_module_load(void)
 {
-    obs_register_source((const struct obs_source_info*)&pixel_match_filter);
+    obs_register_source(&pixel_match_filter);
     init_pixel_match_switcher();
     return true;
 }
