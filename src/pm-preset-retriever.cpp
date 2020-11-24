01127abf45e4482a166b740ebc403d6e3581e3a5
HoneyHazard <8847050+HoneyHazard@users.noreply.github.com>
2020-11-23 18:50:55 -0800

    messing around with libcurl

--- src/pm-preset-retriever.cpp
+++ src/pm-preset-retriever.cpp
@@ -1,3 +1,41 @@
 #include "pm-preset-retriever.hpp"
 
+PmFileRetriever::PmFileRetriever(QString fileUrl, QObject* parent)
+: QObject(parent)
+{
 
+}
+
+void PmFileRetriever::startDownload()
+{
+    m_curlHandle = curl_easy_init();
+
+    curl_easy_setopt(m_curlHandle, CURLOPT_VERBOSE, 1L);
+
+    curl_easy_setopt(m_curlHandle, CURLOPT_PROGRESSFUNCTION, staticProgressFunc);
+    curl_easy_setopt(m_curlHandle, CURLOPT_PROGRESSDATA, this);
+
+    curl_easy_setopt(m_curlHandle, CURLOPT_WRITEFUNCTION, staticWriteFunc);
+    curl_easy_setopt(m_curlHandle, CURLOPT_WRITEDATA, this);
+
+}
+
+int PmFileRetriever::staticProgressFunc(
+    void *clientp,
+    curl_off_t dltotal, curl_off_t dlnow,
+    curl_off_t ultotal, curl_off_t ulnow)
+{
+    return 0;
+}
+
+size_t PmFileRetriever::staticWriteFunc(
+    void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	return size_t();
+}
+
+PmPresetRetriever::PmPresetRetriever(QString xmlUrl, QObject* parent)
+: QObject(parent)
+{
+    curl_global_init(CURL_GLOBAL_ALL);
+}
--- src/pm-preset-retriever.hpp
+++ src/pm-preset-retriever.hpp
@@ -13,23 +13,43 @@ typedef int PmFileRetrieverProgressFunc(void *clientp,
 typedef size_t PmFileRetrieverWriteFunc(
     void *ptr, size_t size, size_t nmemb, void *stream);
 
-class PmFileRetrieverTask
+class PmFileRetriever : public QObject
 {
+    Q_OBJECT
+
 public:
     static const int k_numRetries = 5;
     static const int k_msBeforeRetry = 3000;
 
-    enum ImageRetrieverTask { Idle, Downloading, Halted, Done, Failed };
+    enum FileRetrieverState {
+        Idle, Downloading, RetryPending, Halted, Done, Failed };
+
+    PmFileRetriever(QString fileUrl, QObject *parent = nullptr);
+
+    FileRetrieverState state() const { return m_state; }
+
+    void startDownload();
+    void halt();
+
+signals:
+    void sigFailed(QString error);
+    void sigSucceeded(QByteArray byteArray);
 
 protected:
-    QString m_url;
+    static int staticProgressFunc(void *clientp,
+        curl_off_t dltotal, curl_off_t dlnow,
+        curl_off_t ultotal, curl_off_t ulnow);
+    static size_t staticWriteFunc(
+        void *ptr, size_t size, size_t nmemb, void *stream);
+
+    QString m_fileUrl;
+    QByteArray m_data;
+
+    FileRetrieverState m_state = Idle;
+    CURL *m_curlHandle = nullptr;
 
     int retriesLeft = k_numRetries;
     int msLeftBeforeRetry = k_msBeforeRetry;
-
-    PmFileRetrieverProgressFunc m_progressFunc;
-    PmFileRetrieverWriteFunc m_writeFunc;
-    CURL *m_curlHandle = nullptr;
 };
 
 class PmPresetRetriever : public QObject
@@ -41,23 +61,22 @@ public:
     enum PresetRetrieverState
         { Idle, DownloadingXML, MakeSelection, DownloadingImages };
 
-    // TODO: halted?
-
-public slots:
-    void downloadXML(QString url);
-    void retrievePresets(QList<std::string> presetNames);
+    PmPresetRetriever(QString xmlUrl, QObject *parent = nullptr);
 
 signals:
     // xml download
     void xmlProgress(int percent);
-    void xmlPresetsFailed(QString error);
-    PmMatchPresets xmlPresetsAvailable();
+    void xmlFailed(QString error);
+    QList<QString> xmlPresetsAvailable();
+
+protected slots:
 
     // images download
     void imageProgress(QString presetName, int matchIndex, int percent);
     void imagesFailed(QString error);
 
 protected:
+    int m_numActiveDownloads = 0;
     PresetRetrieverState m_state = Idle;
     QThread *m_thread;
 };
