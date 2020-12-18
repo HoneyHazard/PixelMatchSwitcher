a6260a566267d3aca5506ff6dccc83ceb8d9cb34
HoneyHazard <8847050+HoneyHazard@users.noreply.github.com>
2020-12-17 22:58:53 -0800

    Update about box

--- src/pm-about-box.cpp
+++ src/pm-about-box.cpp
@@ -6,16 +6,21 @@
 #include <obs-module.h>
 
 const char* PmAboutBox::k_aboutText = obs_module_text(
-    "<h2>About Pixel Match Switcher</h2>"
+    "<h1>About Pixel Match Switcher</h1>"
     "<p>Pixel Match Switcher plugin allows switching scenes or toggling scene "
     "     item visibility based on contents of video passing through a Pixel "
     "     Match filter. </p>"
-    "<p>Project's development home is at:<br />"
+    "<p>Source code and installer releases are available at:<br />"
     "    <a href=\"https://github.com/HoneyHazard/PixelMatchSwitcher/\">"
     "           https://github.com/HoneyHazard/PixelMatchSwitcher</a></p>"
     "<p>The plugins presently requires a special fork of OBS to function:"
     "    <a href=\"https://github.com/HoneyHazard/obs-studio-atomic-effects\">"
     "    <br />https://github.com/HoneyHazard/obs-studio-atomic-effects</a></p>"
+
+    "<h2>User Manual</h2>"
+    "<p><a href="
+    "\"https://github.com/HoneyHazard/PixelMatchSwitcher/wiki/User-Manual\">"
+    "https://github.com/HoneyHazard/PixelMatchSwitcher/wiki/User-Manual</a></p>"
 
     "<h2>Special Thank You</h2>"
     "<p>Special Thank You goes out to <a href=\"https://github.com/exeldro\">"
@@ -23,7 +28,7 @@ const char* PmAboutBox::k_aboutText = obs_module_text(
     "    our installer builds:</p>"
     "<p><a href="
     "    \"https://github.com/exeldro/obs-move-transition\">"
-    "    https://github.com/exeldro/obs-move-transition\</a></p>"
+    "    https://github.com/exeldro/obs-move-transition</a></p>"
 
     "<h2>Attributions</h2>"
     "<p>In addition to being a plugin for OBS, Pixel Match Switcher also "
@@ -33,7 +38,7 @@ const char* PmAboutBox::k_aboutText = obs_module_text(
     "    <a href=\"https://icons8.com\">https://icons8.com</a></p>"
     "<p>SMMPTE Color Bars by Denelson83: <br /> <a href="
     "    \"https://commons.wikimedia.org/wiki/File:SMPTE_Color_Bars.svg\">"
-    "    https://commons.wikimedia.org/wiki/File:SMPTE_Color_Bars.svg\</a>"
+    "    https://commons.wikimedia.org/wiki/File:SMPTE_Color_Bars.svg</a>"
 
     "<h2>License</h2>"
     "<p><a href=\"https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html\">"
@@ -55,6 +60,6 @@ PmAboutBox::PmAboutBox(QWidget* parent)
     layout->addWidget(tb);
     setLayout(layout);
 
-    resize(400, 400);
+    resize(400, 600);
     exec();
 }
